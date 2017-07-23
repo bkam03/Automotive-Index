61f40581a74282113351f0e07299cb694302972b
BK <bkam03@yahoo.com>
2017-07-22 19:10:34 -1000

    added indexes to table creation script, fixed bug so everything runs

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -1,16 +1,16 @@
 -- db indexed_cars
+DROP TABLE IF EXISTS car_models;
 
 CREATE TABLE IF NOT EXISTS car_models
 (
- id serial,
+ id serial PRIMARY KEY,
  make_code character varying(125) NOT NULL,
  make_title character varying(125) NOT NULL,
  model_code character varying(125) NOT NULL,
  model_title character varying(125) NOT NULL,
- year integer NOT NULL,
- PRIMARY KEY (id)
-)
+ year integer NOT NULL
+);
 
-CREATE INDEX LAMB ON car_models (make_code);
+CREATE INDEX make_index ON car_models (make_code);
 
 CREATE INDEX YEAR ON car_models ( year );
